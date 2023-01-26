78fca6c5d5aa4a86f2228518ad25d768fa293986
fbm <francis.brunet-manquat@univ-grenoble-alpes.fr>
2023-01-26 21:47:02 +0100

    Début de travail d'affichage sur MainActivity

--- app/src/main/java/fr/iut2/saeprojet/MainActivity.java
+++ app/src/main/java/fr/iut2/saeprojet/MainActivity.java
@@ -4,12 +4,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.List;
 
+import fr.iut2.saeprojet.api.APIClient;
+import fr.iut2.saeprojet.api.APIService;
 import fr.iut2.saeprojet.exempleNOTUSE.api.APIUserClient;
 import fr.iut2.saeprojet.exempleNOTUSE.api.APIUserService;
 import fr.iut2.saeprojet.exempleNOTUSE.entity.UserList;
@@ -19,58 +22,27 @@ import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
-    // View
-    private Button listResourcesView;
-    private Button createUserView;
-    private Button listUsersView;
-    private Button listOffresView;
-    private Button loginView;
+    // API
+    private APIService apiInterface;
 
-    private APIUserService apiInterface;
+    // View
+    private LinearLayout offresView;
+    private LinearLayout candidaturesView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        apiInterface = APIUserClient.getAPIService();
+        // Chargement de l'API
+        apiInterface = APIClient.getAPIService();
 
         // init View
-        listResourcesView = findViewById(R.id.onListRessources);
-        createUserView = findViewById(R.id.onCreateUser);
-        listUsersView = findViewById(R.id.onListUsers);
-        listOffresView = findViewById(R.id.onListOffres);
-        loginView = findViewById(R.id.onLogin);
+        offresView = findViewById(R.id.offres);
+        candidaturesView = findViewById(R.id.candidatures);
 
         //
-        listResourcesView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, ListResourcesActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        //
-        createUserView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, CreateUserActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        //
-        listUsersView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, ListUsersActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        //
-        listOffresView.setOnClickListener(new View.OnClickListener() {
+        offresView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, ListOffresActivity.class);
@@ -79,39 +51,11 @@ public class MainActivity extends AppCompatActivity {
         });
 
         //
-        loginView.setOnClickListener(new View.OnClickListener() {
+        candidaturesView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                Intent intent = new Intent(MainActivity.this, ListOffresActivity.class);
                 startActivity(intent);
-            }
-        });
-
-
-
-        /**
-         POST name and job Url encoded.
-         **/
-        Call<UserList> call3 = apiInterface.doCreateUserWithField("morpheus","leader");
-        call3.enqueue(new Callback<UserList>() {
-            @Override
-            public void onResponse(Call<UserList> call, Response<UserList> response) {
-                UserList userList = response.body();
-                Integer text = userList.page;
-                Integer total = userList.total;
-                Integer totalPages = userList.totalPages;
-                List<UserList.Datum> datumList = userList.data;
-                Toast.makeText(getApplicationContext(), text + " page\n" + total + " total\n" + totalPages + " totalPages\n", Toast.LENGTH_SHORT).show();
-
-                for (UserList.Datum datum : datumList) {
-                    Toast.makeText(getApplicationContext(), "id : " + datum.id + " name: " + datum.first_name + " " + datum.last_name + " avatar: " + datum.avatar, Toast.LENGTH_SHORT).show();
-                }
-
-            }
-
-            @Override
-            public void onFailure(Call<UserList> call, Throwable t) {
-                call.cancel();
             }
         });
 
