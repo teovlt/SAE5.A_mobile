d1db9b5501624ba8968af4253b25dfbe6318b214
fbm <francis.brunet-manquat@univ-grenoble-alpes.fr>
2023-01-26 22:04:13 +0100

    Affichage login et nombres d'offres

--- app/src/main/java/fr/iut2/saeprojet/MainActivity.java
+++ app/src/main/java/fr/iut2/saeprojet/MainActivity.java
@@ -1,10 +1,14 @@
 package fr.iut2.saeprojet;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,6 +17,8 @@ import java.util.List;
 
 import fr.iut2.saeprojet.api.APIClient;
 import fr.iut2.saeprojet.api.APIService;
+import fr.iut2.saeprojet.entity.Offre;
+import fr.iut2.saeprojet.entity.OffreList;
 import fr.iut2.saeprojet.exempleNOTUSE.api.APIUserClient;
 import fr.iut2.saeprojet.exempleNOTUSE.api.APIUserService;
 import fr.iut2.saeprojet.exempleNOTUSE.entity.UserList;
@@ -59,5 +65,46 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //
+        refreshLogin();
+        refreshOffres();
+    }
+
+    private void refreshLogin() {
+
+        //
+        SharedPreferences sharedPref = getApplicationContext().getSharedPreferences("MyPref", Context.MODE_PRIVATE);
+        String login = sharedPref.getString(getString(R.string.login_key), "no login");
+
+        //
+        TextView loginView = findViewById(R.id.login);
+        loginView.setText(login);
+    }
+
+    private void refreshOffres() {
+
+        //
+        SharedPreferences sharedPref = getApplicationContext().getSharedPreferences("MyPref", Context.MODE_PRIVATE);
+        String token = sharedPref.getString(getString(R.string.token_key), "no token");
+
+        //
+        Call<OffreList> call = apiInterface.doGetOffres("Bearer " + token);
+        call.enqueue(new Callback<OffreList>() {
+            @Override
+            public void onResponse(Call<OffreList> call, Response<OffreList> response) {
+
+                //
+                TextView nbOffresView = findViewById(R.id.textView8);
+                OffreList offres = response.body();
+                nbOffresView.setText(String.valueOf(offres.offres.size()) + " " + nbOffresView.getText().toString());
+            }
+
+            @Override
+            public void onFailure(Call<OffreList> call, Throwable t) {
+                call.cancel();
+                Log.e("TAG",t.getMessage());
+
+            }
+        });
     }
 }
